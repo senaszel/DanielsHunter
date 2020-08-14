f4cd6583eef800d1b1a13852e978ca78b571f9c1
Marek Stanisław Meyer <marekstanislawmeyer@gmail.com>
2020-08-15 00:40:26 +0200

    DanielsHunter theGame

---
+++ DanielsHunter/Board.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace DanielsHunter
+{
+    static class Board
+    {
+        public static int Height { get => 20; }
+        public static int Width { get => 50; }
+        static int Offset { get => 4; }
+
+
+        static string[] View { get; set; }
+
+        public static string[] Generate()
+        {
+            View = new string[Height + 1];
+
+            Console.WriteLine(string.Concat(Enumerable.Repeat("\r\n", Offset)));
+            for (int i = 0; i <= Height; i++)
+            {
+                if (i == 0 || i == Height)
+                {
+                    View[i] = string.Concat(new string(' ', Offset * 2), new string('#', Width));
+                }
+                else
+                {
+                    View[i] = string.Concat(new string(' ', Offset * 2), '#', new string(' ', Width - 2), '#');
+                }
+            }
+            return View;
+        }
+
+        public static string[] PlaceDaniel()
+        {
+            Random random = new Random();
+
+            int treasureX = random.Next(9, 57);
+            int treasureY = random.Next(1, 20);
+            if (View[treasureY].Substring(treasureX, 1) != "@" || View[treasureY].Substring(treasureX, 1) != "#")
+            {
+                View[treasureY] = View[treasureY].Insert(treasureX, "d").Remove(treasureX + 1, 1);
+            }
+            else PlaceDaniel();
+
+            return View;
+        }
+
+        internal static string[] GrowTrees(string[] view, int userX, int userY)
+        {
+            view[userY - 1] = view[userY - 1].Insert(userX - 1, "#").Remove(userX + 1, 1);
+            view[userY + 1] = view[userY + 1].Insert(userX - 1, "#").Remove(userX + 1, 1);
+
+            view[userY - 1] = view[userY - 1].Insert(userX + 1, "#").Remove(userX + 2, 1);
+            view[userY + 1] = view[userY + 1].Insert(userX + 1, "#").Remove(userX + 2, 1);
+            return view;
+        }
+
+        public static void Draw(string[] board)
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                Console.WriteLine(board[i]);
+            }
+        }
+
+
+    }
+}
---
+++ DanielsHunter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DanielsHunter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.SetWindowSize(85,32);
+            Game.Set();
+            Game.Start();
+
+            int exit3 = 0;
+            ConsoleKey exit;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(string.Concat("\r\n\r\n",new string(' ',20),Game.Outcome));
+                Console.WriteLine("\r\n\r\n3 x Escape to Quit");
+                Console.WriteLine();
+                exit = Console.ReadKey().Key;
+                if (exit == ConsoleKey.Escape) { exit3 += 1; }
+
+            } while (exit3 < 3);
+        }
+    }
+}
